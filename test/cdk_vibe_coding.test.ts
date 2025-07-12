cab2508e2c6dc891e823a9e954b50036d75508b4
raiha <68338762+raihalea@users.noreply.github.com>
2025-07-12 13:14:20 +0900

    Initial commit

---
+++ test/cdk_vibe_coding.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVibeCoding from '../lib/cdk_vibe_coding-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_vibe_coding-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVibeCoding.CdkVibeCodingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
